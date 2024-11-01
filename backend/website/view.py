bd7dde1ddcb87e123e4a1ae4a6586c3274d51a84
Mayank <mayankcryptobyte@gmail.com>
2024-11-02 04:21:06 +0530

    removed clutter

--- backend/website/view.py
+++ backend/website/view.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 
 from django.views.decorators.csrf import csrf_exempt
