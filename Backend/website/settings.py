324cc0eb09a32f428c6fcd424870b63d4f7f4bb9
Mayank <mayankcryptobyte@gmail.com>
2024-11-01 20:57:00 +0530

    Updated Database

--- Backend/website/settings.py
+++ Backend/website/settings.py
@@ -1,11 +1,11 @@
 from pathlib import Path
+from dotenv import load_dotenv
 
+# Load .env file
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5pht+z*(%lh#!k@u^39(ue97fzz&2reme%*^ep$+nq6byta%8p'
@@ -62,11 +62,15 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
