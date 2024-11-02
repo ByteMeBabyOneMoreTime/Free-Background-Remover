2f6f15991ee42a4d7fdf712301857f1fc044a2d5
Mayank <mayankcryptobyte@gmail.com>
2024-11-03 01:14:34 +0530

    Debug ture

--- backend/website/settings.py
+++ backend/website/settings.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
