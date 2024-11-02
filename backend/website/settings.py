83c607ea1edb3bf8ee664126dc9118e550cc6021
Mayank <mayankcryptobyte@gmail.com>
2024-11-03 01:59:14 +0530

    Added Static config

--- backend/website/settings.py
+++ backend/website/settings.py
@@ -3,14 +3,14 @@ import os
 from dotenv import load_dotenv
 
 # Load .env file
-# load_dotenv()
-# # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
