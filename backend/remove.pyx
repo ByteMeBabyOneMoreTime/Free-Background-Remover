d1c52b3f4ae7e4af377808841b2aac681f00ba3e
Mayank <mayankcryptobyte@gmail.com>
2024-11-03 01:23:28 +0530

    Debug ture

--- backend/remove.pyx
+++ backend/remove.pyx
@@ -15,7 +15,7 @@ def rem(request, api):
         try:
             img = Image.open(image_file)
             # Remove the background
-            output_img = remove(img, model='isnet-general-use')
+            output_img = remove(img, model='silueta')
 
             # Save the output image to a BytesIO object
             output_io = io.BytesIO()
